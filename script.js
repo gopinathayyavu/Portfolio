0546522d5098720dc2faf40e21d3d71350cfbe4f
gopinathayyavu <gopinath0028@gmail.com>
2024-08-07 22:46:12 +0530

    update the style

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+var  menubtn = document.getElementById("menubtn")
+var  sidenav = document.getElementById("sidenav")
+var  menu = document.getElementById("menu")
+
+sidenav.style.right = "-250px";
+
+menubtn.onclick = function(){
+    if(sidenav.style.right == "-250px"){
+        sidenav.style.right = "0";
+        menu.src ="assets/menuclose.png"
+    }
+    else{
+        sidenav.style.right = "-250px";
+        menu.src ="assets/menuopen.png"
+    }
+}
